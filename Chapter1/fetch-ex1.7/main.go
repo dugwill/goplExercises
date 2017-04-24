530a1b07f017071004394635cf2a5e521d847a67
dugwill <douglaswi1@comcast.net>
2017-04-23 21:24:43 -0600

    Added Exercise 1.7

---
+++ Chapter1/fetch-ex1.7/main.go
@@ -0,0 +1,44 @@
+// Original work
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// gopl.io Exercise 1.6
+// Modifications Copyright © 2017 Douglas Will
+// License: https://creativecommons.org/licenses/by-sa/4.0/
+// See page 16.
+//!+
+
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		// For exdersize 1.7. the following section replaces orignal buffer print section
+		// uses io.Copy rather than a buffer for printing to os.Stdout
+		nbytes, err := io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close() //Close the resource
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("\nRead %d\n", nbytes)
+
+	}
+}
+
+//!-
