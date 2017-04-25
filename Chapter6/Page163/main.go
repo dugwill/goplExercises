b9615bbedc0996367119d787fd52a8bfb53bbc1f
dugwill <douglaswi1@comcast.net>
2017-04-24 18:46:47 -0600

    Added ex5.19 and a example from Chapter 6

---
+++ Chapter6/Page163/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// This var statement groups the struct and the map together in
+// a single var 'cache'. The methods of 'sync.mutex' are
+// promoted to 'cache' through the unnamed struct type
+var cache = struct {
+	sync.Mutex // guards mapping
+	mapping    map[string]string
+}{
+	mapping: make(map[string]string),
+}
+
+func main() {
+
+	// Since the 'Lock' method fo 'sync.Mutex' is available to 'cache'
+	// we can use it to lock and unlock the map.
+	cache.Lock()
+	cache.mapping["bob"] = "jones"
+	cache.mapping["joe"] = "smith"
+	cache.Unlock()
+
+	fmt.Printf("%v\n", Lookup("joe"))
+
+}
+
+// Since the 'cache' is a package level var, we can use the
+// methods in the package funcs as well
+
+func Lookup(key string) string {
+	cache.Lock()
+	v := cache.mapping[key]
+	cache.Unlock()
+	return v
+}
