81e33c65e5a65ff3f0edb21e3d199f7bc6c72c60
dugwill <douglaswi1@comcast.net>
2017-05-16 17:19:28 -0600

    Working on 7.10

--- Chapter7/ex7.10/main.go
+++ Chapter7/ex7.10/main.go
@@ -12,11 +12,13 @@ import (
 
 func main() {
 
-	input := []int{1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1}
+	input := palitest{1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1}
 
-	if sort.Sort(sort.Reverse(sort.IntSlice(input))) {
+	sort.Sort(sort.Reverse(input))
+	fmt.Println(input)
+	/*{
 		fmt.Println(input + " is a Palindrome")
-	}
+	}*/
 
 }
 
@@ -24,3 +26,9 @@ func IsPalindrome(s sort.Interface) bool {
 
 	return true
 }
+
+type palitest []int
+
+func (x palitest) Len() int            { return len(x) }
+func (x palitest) Less(i, j, int) bool { return i < j }
+func (x palitest) Swap(i, j) int       { x[i], x[j] = x[j], x[i] }
