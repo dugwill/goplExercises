a724179ef410b1a9a142c7c756559f3991151419
dugwill <douglaswi1@comcast.net>
2017-05-24 06:29:53 -0600

    Still on 7.1

--- Chapter7/ex7.10/main.go
+++ Chapter7/ex7.10/main.go
@@ -12,13 +12,15 @@ import (
 
 func main() {
 
-	input := palitest{1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1}
+	input := palitest{7, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1}
 
-	sort.Sort(sort.Reverse(input))
+	sort.Sort(input)
 	fmt.Println(input)
-	/*{
-		fmt.Println(input + " is a Palindrome")
-	}*/
+
+	input = palitest{7, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1}
+
+	sort.Reverse(input)
+	fmt.Println(input)
 
 }
 
@@ -29,6 +31,6 @@ func IsPalindrome(s sort.Interface) bool {
 
 type palitest []int
 
-func (x palitest) Len() int            { return len(x) }
-func (x palitest) Less(i, j, int) bool { return i < j }
-func (x palitest) Swap(i, j) int       { x[i], x[j] = x[j], x[i] }
+func (x palitest) Len() int           { fmt.Println("Len"); return len(x) }
+func (x palitest) Less(i, j int) bool { fmt.Println("Less"); return x[i] < x[j] } //This tests if i<j and returns t/f
+func (x palitest) Swap(i, j int)      { fmt.Println("Swap"); x[i], x[j] = x[j], x[i] }
