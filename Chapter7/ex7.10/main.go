d65d895218eadb451a10ddf0bf182c3bfaa7f273
dugwill <douglaswi1@comcast.net>
2017-06-04 15:57:41 -0600

    Finished Excersize 7.10

--- Chapter7/ex7.10/main.go
+++ Chapter7/ex7.10/main.go
@@ -1,34 +1,35 @@
 // gopl.io Exercise 7.10
 // License: https://creativecommons.org/licenses/by-sa/4.0/
 
-// ***** This excercise is not finished 88888
-
 package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
 
-	input := palitest{1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1}
-
-	sort.Sort(sort.Reverse(input))
-	fmt.Println(input)
-	/*{
-		fmt.Println(input + " is a Palindrome")
-	}*/
+	fmt.Println(isPalindrome([]byte("racecar")))                   //true
+	fmt.Println(isPalindrome([]byte("sore was I ere I saw eros"))) //true
+	fmt.Println(isPalindrome([]byte("This is not a Palindrome")))  //false
 
 }
 
-func IsPalindrome(s sort.Interface) bool {
+func isPalindrome(s palitest) bool {
+	//Setup a loop, i is index for left bytes, j is index for right bytes
 
+	for i, j := 0, s.Len()-1; i < j; i, j = i+1, j-1 {
+		if !s.Less(i, j) && !s.Less(j, i) { //if i!<j and j!<i, then they must be equal
+			continue //if equal continue
+		} else {
+			return false //If not equal then return false
+		}
+	}
 	return true
 }
 
-type palitest []int
+type palitest []byte
 
-func (x palitest) Len() int            { return len(x) }
-func (x palitest) Less(i, j, int) bool { return i < j }
-func (x palitest) Swap(i, j) int       { x[i], x[j] = x[j], x[i] }
+func (x palitest) Len() int           { return len(x) }
+func (x palitest) Less(i, j int) bool { return (x[i] < x[j]) }
+func (x palitest) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
