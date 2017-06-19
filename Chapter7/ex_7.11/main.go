b7cc727b06d0b6919fc871704bff9c13fcf54b52
dugwill <douglaswi1@comcast.net>
2017-06-19 15:30:28 -0600

    Adding comments to ex 7.11

--- Chapter7/ex_7.11/main.go
+++ Chapter7/ex_7.11/main.go
@@ -1,3 +1,4 @@
+// Original Work
 // Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 
@@ -6,7 +7,13 @@
 // Http4 is an e-commerce server that registers the /list and /price
 // endpoint by calling http.HandleFunc.
 
-////////////// This excersize is not done //////////////
+// Modifications Copyright © 2017 Douglas Will
+// License: https://creativecommons.org/licenses/by-sa/4.0/
+
+// ***********  This Exersize is finished   ********/
+// Added handlers for Create, update, and delete.
+// I wasn't sure what was meant by read since the handler
+// price already read the DB for the price of a particular item
 
 package main
 
@@ -23,8 +30,9 @@ func main() {
 	db := database{"shoes": 50, "socks": 5}
 	http.HandleFunc("/list", db.list)
 	http.HandleFunc("/price", db.price)
-	http.HandleFunc("/add", db.add)
+	http.HandleFunc("/create", db.create) // New Handler for crating an new item
 	http.HandleFunc("/update", db.update)
+	http.HandleFunc("/delete", db.delete)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
@@ -52,7 +60,7 @@ func (db database) price(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func (db database) add(w http.ResponseWriter, req *http.Request) {
+func (db database) create(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
 	price := req.URL.Query().Get("price")
 
@@ -83,4 +91,18 @@ func (db database) update(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "no such item: %q\n", item)
 	}
 
+}
+
+func (db database) delete(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+
+	_, ok := db[item]
+
+	if ok {
+		delete(db, item)
+		fmt.Fprintf(w, "Successfully removed %q\n", item)
+	} else {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+	}
 }
